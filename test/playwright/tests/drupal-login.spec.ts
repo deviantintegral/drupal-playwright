5f485e9c533efd6d622306d6019d722256bb2674
Andrew Berry <andrew@furrypaws.ca>
2023-03-22 20:30:56 -0400

    Document some example test pitfalls

--- test/playwright/tests/drupal-login.spec.ts
+++ test/playwright/tests/drupal-login.spec.ts
@@ -1,5 +1,15 @@
 import { test, expect } from '@playwright/test';
 import {Page} from "playwright";
+
+// Olivero offers a great example of how tests can "randomly fail" when adding
+// a mobile viewport. The menu at mobile viewports is collapsed. If you have a
+// single test that tries to click on "Log in", that link won't be visible at
+// mobile.
+//
+// We use test groups with grep and grepInvert in the configuration to handle
+// viewport specific tests.
+//
+// https://stackoverflow.com/a/74385433/7982687
 test.describe('mobile', () => {
   test.beforeEach(async ({page}) => {
     await page.goto('https://drupal-playwright.ddev.site/');
@@ -19,6 +29,10 @@ test.describe('desktop', () => {
   });
 });
 
+// If you run this many times quickly, eventually Drupal's flood protection
+// will kick in and throw an error. This is where making sure you have a
+// clean site to test against is important, because even this test that
+// supposedly has no side effects does!
 async function testInvalidLogin(page: Page) {
   await page.getByRole('link', {name: 'Log in'}).click();
   await page.getByLabel('Username').fill('test');
